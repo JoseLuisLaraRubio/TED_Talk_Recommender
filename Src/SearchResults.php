609fd7b9834a36070eb218288f52deb9861c8074
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2025-05-24 18:29:43 -0600

    Added datasets and defined proyect structure

---
+++ Src/SearchResults.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search results</title>
+</head>
+<body>
+    <div class="return-home">
+
+    </div>
+
+    <div class="search-bar">
+
+    </div>
+
+    <div class="results">
+
+    </div>
+</body>
+</html>
--- Src/Video.php
+++ Src/Video.php
@@ -1,6 +1,5 @@
-<?php 
-
-
+<?php
+    
 ?>
 
 <!DOCTYPE html>
@@ -8,25 +7,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ted talks</title>
+    <title>Talk Title</title>
 </head>
 <body>
+    <div class="return-home">
+
+    </div>
+
     <div class="search-bar">
 
     </div>
 
+    <div class="video-player">
+
+    </div>
+    <div class="rate">
+
+    </div>
+
     <div class="recommendations">
-        <div class="you-may-like">
-    
+        <div class="more-from-event">
+             
         </div>
-        <div class="recent-talks">
-    
-        </div>
-        <div class="similar-users-watched">
-    
-        </div>
-        <div class="because-you-watched">
-    
+        <div class="similar-talks">
+             
         </div>
     <div>
 </body>
