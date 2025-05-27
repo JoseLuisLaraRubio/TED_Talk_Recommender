110a4c8ea3f351229db71b90f3d542613590273e
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2025-05-27 10:35:56 -0600

    style: :lipstick: update UI

--- Src/Index.php
+++ Src/Index.php
@@ -6,6 +6,18 @@
 
     $popular_talks = get_most_popular_talks(20);
     $user_recommended_talks = get_recommendations_by_user($user_id, 20) ?? get_recommendations_by_user(0, 20);
+
+    $recommendations = [];
+
+    foreach (array_slice($user_recommended_talks, 0, 10) as $talk) {
+        $title = htmlspecialchars($talk['title']);
+        $similar_talks = get_recommendations_by_content($title, 5);
+
+        if (!empty($similar_talks)) {
+            $recommendations[$title] = $similar_talks;
+        }
+    }
+
     $random_talk = get_random_talk();
 ?>
 
@@ -63,8 +75,33 @@
         </div>
 
         <div class="because-you-watched">
-                
+            <?php foreach ($recommendations as $talkTitle => $similarTalks): ?>
+                <?php if ($talkTitle !== null && $talkTitle !== ''): ?>
+                    <div class="recommendation-row">
+                        <h2>Because other users watched: <?= htmlspecialchars($talkTitle) ?></h2>
+                        <div class="similar-talks-row">
+                            <?php foreach ($similarTalks as $talk): ?>
+                                <div class="talk">
+                                    <?php
+                                    $embedUrl = str_replace('www.ted.com', 'embed.ted.com', $talk['url']);
+                                    ?>
+                                    <div><iframe src="<?= $embedUrl; ?>" autoplay="0" frameborder="0" sandbox="allow-same-origin"></iframe></div>
+                                    <div class="title"><?= htmlspecialchars($talk['title']); ?></div>
+                                    <div class="speaker">by <?= htmlspecialchars($talk['main_speaker']); ?></div>
+                                    <div class="views"><?= number_format((int)$talk['views']) ?> views</div>
+                                    <br>
+                                    <div class="video">
+                                        <a href="watch.php?title=<?= urlencode($talk['title']); ?>&url=<?= urlencode($talk['url']); ?>&user_id=<?= urlencode($user_id); ?>">Watch Talk</a>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
         </div>
+
+
 
         <div class="random-recommendation">
             <h2>Want some random recommendation?</h2>
